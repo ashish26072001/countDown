d8d367e08bf13ef35eedb61e1a0e1e920ee91abd
anjubora <ashishofficial026@gmail.com>
2024-07-08 22:32:45 +0530

    first commit

---
+++ src/component/TimerInput.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+export default function TimerInput({handleInput,handleStart}) {
+  return (
+    <div className='input-container'>
+        <div className='input-box'>
+            <input
+                onChange={handleInput}
+                id="hours" placeholder='HH' />
+            <input
+                onChange={handleInput}
+                id="minutes" placeholder='MM' />
+            <input
+                onChange={handleInput}
+                id="seconds" placeholder='SS' />
+        </div>
+        <button
+            onClick={handleStart}
+            className='timer-button'>Start</button>
+    </div>
+)
+}
+
+
